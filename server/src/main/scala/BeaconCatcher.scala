3f34a031eef5ff8901e650c985a924cb40d25dbd
sxend <arimitsu.k+github@gmail.com>
2016-01-16 20:55:55 +0900

    code format

--- server/src/main/scala/BeaconCatcher.scala
+++ server/src/main/scala/BeaconCatcher.scala
@@ -2,7 +2,7 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.marshalling.Marshal
 import akka.http.scaladsl.model.StatusCodes._
-import spray.json.{JsObject, DefaultJsonProtocol}
+import spray.json.{ JsObject, DefaultJsonProtocol }
 import spray.json._
 import scala.concurrent.Future
 import akka.http.scaladsl.Http
@@ -11,8 +11,8 @@ import akka.stream.ActorMaterializer
 import DefaultJsonProtocol._
 
 /**
-  * Created by sxend on 1/15/16.
-  */
+ * Created by sxend on 1/15/16.
+ */
 object BeaconCatcher {
   def main(args: Array[String]): Unit = {
 
