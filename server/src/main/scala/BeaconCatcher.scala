60c2dbbc4a5e28d5505571694602112f3d1ff6c7
sxend <arimitsu.k+github@gmail.com>
2016-01-16 16:22:52 +0900

    create endpoint

--- server/src/main/scala/BeaconCatcher.scala
+++ server/src/main/scala/BeaconCatcher.scala
@@ -1,12 +1,34 @@
+import akka.actor.ActorSystem
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.marshalling.Marshal
+import akka.http.scaladsl.model.StatusCodes._
+import spray.json.{JsObject, DefaultJsonProtocol}
+import spray.json._
+import scala.concurrent.Future
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import DefaultJsonProtocol._
+
 /**
   * Created by sxend on 1/15/16.
   */
 object BeaconCatcher {
   def main(args: Array[String]): Unit = {
 
+
+    implicit val system = ActorSystem()
+    implicit val materializer = ActorMaterializer()
+    val route =
+      get {
+        path("beacon") {
+          parameter("message") { (message) =>
+            println(message)
+            complete(NoContent)
+          }
+        }
+      }
+
+    Http().bindAndHandle(route, "localhost", 9000)
   }
-}
-
-class BeaconHandler {
-
 }
