bc06dbc50723f17ae7b8a9655865dccd3bf26754
sxend <arimitsu.k+github@gmail.com>
2016-01-17 17:49:07 +0900

    beacon test script

---
+++ client/demo/bookmarklet.js
@@ -0,0 +1,4 @@
+javascript:(function(){
+var script = document.createElement('script');script.src = "http://localhost:8000/scripts/browsing-beacon.js";document.body.appendChild(script);
+var script = document.createElement('script');script.src = "http://localhost:8000/scripts/example.js";document.body.appendChild(script);
+})();
