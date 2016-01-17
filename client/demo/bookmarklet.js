a6503cf3361351002df7846a9319d897c3069f1b
sxend <arimitsu.k+github@gmail.com>
2016-01-17 18:08:34 +0900

    move examples

--- client/demo/bookmarklet.js
+++ client/demo/bookmarklet.js
@@ -1,4 +1,4 @@
 javascript:(function(){
-var script = document.createElement('script');script.src = "http://localhost:8000/scripts/browsing-beacon.js";document.body.appendChild(script);
-var script = document.createElement('script');script.src = "http://localhost:8000/scripts/example.js";document.body.appendChild(script);
+var script = document.createElement('script');script.src = "http://localhost:8000/dist/browsing-beacon.js";document.body.appendChild(script);
+var script = document.createElement('script');script.src = "http://localhost:8000/examples/index.js";document.body.appendChild(script);
 })();
