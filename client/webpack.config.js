1c23e29841df32f739ad493cc1111e79cc981f0b
sxend <arimitsu.k+github@gmail.com>
2016-01-17 17:49:37 +0900

    add minify module

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,8 +1,16 @@
+var webpack = require('webpack');
 module.exports = {
-    context: __dirname + "/scripts",
-    entry: "./index.js",
-    output: {
-        path: __dirname + "/dist",
-        filename: "browsing-beacon.js"
-    }
+  context: __dirname + "/scripts",
+  entry: "./index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "browsing-beacon.js"
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
 };
