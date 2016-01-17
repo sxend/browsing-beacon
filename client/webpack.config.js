c0c75ac216a292a337dce363719350952d2c188c
sxend <arimitsu.k+github@gmail.com>
2016-01-17 18:05:40 +0900

    switch build target

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,21 +1,26 @@
 var webpack = require('webpack');
-module.exports = {
+var isProductionBuild = process.argv.indexOf('-production') > 0;
+
+var minifyPlugin = new webpack.optimize.UglifyJsPlugin({
+  compress: {
+    warnings: false
+  }
+});
+
+var plugins = isProductionBuild ? [minifyPlugin] : [];
+
+var config = {
   context: __dirname + "/scripts",
   entry: "./index.js",
   output: {
     path: __dirname + "/dist",
     filename: "browsing-beacon.js"
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    })
-  ],
+  plugins: plugins,
   loaders: [{
     test: /\.(js)$/,
     exclude: /node_modules/,
     loader: 'babel?presets[]=es2015'
   }]
 };
+module.exports = config;
