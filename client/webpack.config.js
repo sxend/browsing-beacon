3c9189535b147495960fbb2a3d33164ff2b346d0
sxend <arimitsu.k+github@gmail.com>
2016-01-17 17:41:22 +0900

    add build

---
+++ client/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    context: __dirname + "/scripts",
+    entry: "./index.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "browsing-beacon.js"
+    }
+};
