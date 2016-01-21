79a50c9edbdae1d0cf6b7b40659888a609f2a076
sxend <arimitsu.k+github@gmail.com>
2016-01-21 20:18:36 +0900

    fix to ga like init script

---
+++ client/examples/initial-script.js
@@ -0,0 +1,12 @@
+(function(window, document, script, beaconjs, name, tag, beforeTag) {
+  window['BrowsingBeaconObject'] = name,
+    window[name] = window[name] || function() {
+      (window[name].q = window[name].q || []).push(arguments);
+    },
+    window[name].l = 1 * new Date(),
+    tag = document.createElement(script),
+    tag.async = 1,
+    tag.src = beaconjs,
+    beforeTag = document.getElementsByTagName(script)[0];
+  beforeTag.parentNode.insertBefore(tag, beforeTag);
+})(window, document, 'script', '//localhost:8000/dist/browsing-beacon.js', 'bb');
