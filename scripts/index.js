b2302084c6a5c12dc06ae19f72a3276358896a24
sxend <arimitsu.k+github@gmail.com>
2016-01-24 02:31:58 +0900

    use deplicate method

--- scripts/index.js
+++ scripts/index.js
@@ -4,5 +4,5 @@ let name = window['BrowsingBeaconObject'];
 let bb = window[name];
 bb.log = console.log.bind(console);
 bb.q.forEach(function(command){
-  if (command[0] == 'configure') configure(bb, command.slice(1, command.length));
+  if (command[0] == 'configure') configure(bb, [].slice.call(command).slice(1, command.length));
 })
