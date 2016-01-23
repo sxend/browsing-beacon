0906b5d268276b071acf04c448a7421a68bd27ad
sxend <arimitsu.k+github@gmail.com>
2016-01-24 03:13:30 +0900

    move log bind

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
+bb.log = console.log.bind(console);
 let Events = require('./events');
 let configure = require('./commands/configure');
-console.log(configure);
+bb.log(configure);
 let name = window['BrowsingBeaconObject'];
 let bb = window[name];
-bb.log = console.log.bind(console);
+
 bb.q.forEach(function(command){
   if (command[0] == 'configure') configure(bb, [].slice.call(command).slice(1, command.length));
 })
