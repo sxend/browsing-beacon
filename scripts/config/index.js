50cf7f6ba8900e9209316782a879bde75f8ca7e8
sxend <arimitsu.k+github@gmail.com>
2016-01-26 11:00:25 +0900

    fix config loading

--- scripts/config/index.js
+++ scripts/config/index.js
@@ -1,10 +1,8 @@
 import prd from './prd';
 import stg from './stg';
+import bb from '../';
 export default class Config {
   static getConfig (){
-    isProduction() ? prd : stg;
-  }
-  static isProduction() {
-    return !!process.env.TO_PRODUCTION;
+    bb.isProduction() ? prd : stg;
   }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,30 @@
 let name = window['BrowsingBeaconObject'];
 let bb = window[name];
-bb.log = !!bb.__debug__ ? console.log.bind(console) : function() {};
 
-import commands from './commands';
-import events from './events';
-bb.cm = commands;
-bb.ev = events;
-
-setInterval(function() {
-  let length = bb.q.length;
-  for (let i = 0; i < length; i++) {
-    let command = bb.q.shift();
-    if(!command || command.length <= 0) {
-      return;
-    }
-    let handler = bb.cm[command[0]];
-    if (handler) {
-      handler(bb, [].slice.call(command).slice(1, command.length));
-    }
+function initialize() {
+  bb.isProduction = function() {
+    return !bb.__ENVIRONMENT__ || bb.__ENVIRONMENT__ == 'prd';
   }
-}, 10);
+  bb.log = bb.isProduction() ? function() {} : console.log.bind(console);
+
+  import commands from './commands';
+  import events from './events';
+  bb.cm = commands;
+  bb.ev = events;
+
+  setInterval(function() {
+    let length = bb.q.length;
+    for (let i = 0; i < length; i++) {
+      let command = bb.q.shift();
+      if (!command || command.length <= 0) {
+        return;
+      }
+      let handler = bb.cm[command[0]];
+      if (handler) {
+        handler(bb, [].slice.call(command).slice(1, command.length));
+      }
+    }
+  }, 10);
+}
+initialize();
+export default bb;
