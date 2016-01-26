d8bf0f742bdee78011329613b17954dfc664cc86
sxend <arimitsu.k+github@gmail.com>
2016-01-26 19:37:53 +0900

    add slice

--- scripts/commands/watch.js
+++ scripts/commands/watch.js
@@ -1,7 +1,7 @@
 export default function(bbevent, callback) {
   let bb = this;
   let elements = document.querySelectorAll(bbevent.condition.querySelector);
-  elements.forEach(function(element) {
+  [].slice.call(elements).forEach(function(element) {
     bbevent.watcher(element, callback);
   });
 }
