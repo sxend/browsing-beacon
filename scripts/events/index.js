28c55877d2319fc174eb07a35926cf522451d2b5
sxend <arimitsu.k+github@gmail.com>
2016-01-26 19:52:51 +0900

    refactor

--- scripts/events/index.js
+++ scripts/events/index.js
@@ -7,39 +7,3 @@ export default {
   'Click': Click,
   'Mouseover': Mouseover
 }
-class Events {
-  constructor (config) {
-    this.emitter = new Emitter(config);
-
-    loadDefaultEvent(this, config);
-  }
-
-  register(eventDefinition) {
-    Events[eventDefinition.name] = createEventClass(eventDefinition);
-    return Events[eventDefinition.name];
-  }
-  watch(target, callback) {
-    let elements = document.querySelectorAll(target.querySelector);
-
-
-    [].slice.call(elements).forEach(function(element) {
-      let context = {};
-      target.handler.call(context, element, function(event) {
-        callback.call(context, element, event, messageCallback);
-      });
-    });
-  }
-}
-
-function loadDefaultEvent(events) {
-  this.InView = InView;
-}
-function createEventClass(eventDefinition) {
-  return function(config) {
-    return new Event(eventDefinition, config);
-  }
-}
-
-function messageCallback(emitter, message) {
-  emitter.emit(message);
-}
