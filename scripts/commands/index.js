6299ec60fac83be93d0b041daee8bc394e578422
sxend <arimitsu.k+github@gmail.com>
2016-01-26 19:39:19 +0900

    add watch command

--- scripts/commands/index.js
+++ scripts/commands/index.js
@@ -1,6 +1,8 @@
 import configure from './configure';
 import send from './send';
+import watch from './watch';
 export default {
   'configure': configure,
-  'send': send
+  'send': send,
+  'watch': watch
 }
