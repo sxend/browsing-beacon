95188d011ff77eb3dc94a1a759377d1e93b11107
sxend <arimitsu.k+github@gmail.com>
2016-01-26 21:10:08 +0900

    add inview

--- scripts/events/inview.js
+++ scripts/events/inview.js
@@ -1,25 +1,74 @@
-
-module.exports = {
-  name: 'InView',
-  handler: function(element, callback) { // FIXME
-    element.addEventListener('click', function(event) {
-      callback(event);
-    });
-  }
-};
 import BBEvent from './bbevent';
+import Rectangle from './rectangle';
+
 export default class InView extends BBEvent {
-  constructor(querySelector, context) {
-    super(querySelector, context);
-    this.querySelector = querySelector;
+  constructor(condition) {
+    super(condition);
   }
-  listen(element, context) {
-    let event = this;
-    element.addEventListener('click', function(domEvent) {
+  watch(callback) {
 
-    });
-    element.addEventListener('mouseover', function(domEvent){
+    try {
+      super.getElements().forEach(function(element) {
+        let inViewManager = new InViewManager(element, callback);
+        element.addEventListener('mouseover', function(ev) {
+          callback(null, ev);
+        });
+      });
+    } catch (e) {
+      callback(e);
+    }
+  }
+}
 
-    });
+class InViewInstrument {
+  static inViewThresholdMillis() {
+    return 1000;
+  };
+  static getViewportRect(doc) {
+    let l = doc.documentElement.scrollLeft || doc.body.scrollLeft;
+    let t = doc.documentElement.scrollTop || doc.body.scrollTop;
+    let w = doc.body.clientWidth;
+    let h = doc.body.clientHeight;
+
+    return new Rectangle(l, t, w, h);
+  };
+}
+
+class InViewManager {
+  constructor(element, callback, handler) {
+    this.element = element;
+    this.callabck = callback;
+    this.handler = createInViewInstrumentHandler(this);
+    this.inView = false;
+    this.timerId = 0;
+  }
+}
+
+function createInViewInstrumentHandler(inViewManager) {
+  return function(e) {
+    if (inViewManager.inView) {
+      return;
+    }
+    switch (e.type) {
+      case "load":
+      case "resize":
+      case "scroll":
+        let viewportRect = InViewInstrument.getViewportRect(window.top.document);
+        let adClientRect = inViewManager.element.getBoundingClientRect();
+        let adRect = new Rectangle(
+          adClientRect.left + window.scrollX,
+          adClientRect.top + window.scrollY,
+          adClientRect.width,
+          adClientRect.height);
+
+        // 重なっている時のみ計算
+        // 交差している面積が広告の50%以上あるか
+        if (viewportRect.getIntersectingArea(adRect) / adRect.getArea() >= 0.5) {
+          InViewInstrument.startInViewTimer(inViewManager);
+        } else {
+          InViewInstrument.clearInViewTimer(inViewManager);
+        }
+        break;
+    }
   }
 }
