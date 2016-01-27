02c9f1cc7f06a13a4ef5fb87e1b76d0340251103
sxend <arimitsu.k+github@gmail.com>
2016-01-27 12:30:51 +0900

    fix typecheck

--- scripts/utils/type-check.js
+++ scripts/utils/type-check.js
@@ -2,10 +2,14 @@ export function isFunction(o) {
   return "function" == typeof o;
 }
 
+export function isNumber(o) {
+  return "number" == typeof o;
+}
+
 export function isArray(o) {
   return "[object Array]" == Object.prototype.toString.call(Object(o));
 }
 
 export function isString(o) {
-  return void 0 != o && -1 < (a.constructor + "").indexOf("String");
+  return void 0 != o && -1 < (o.constructor + "").indexOf("String");
 }
