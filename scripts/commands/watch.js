f0963ca6346aff5cb77eb8147c2aec356a646369
sxend <arimitsu.k+github@gmail.com>
2016-01-29 16:16:51 +0900

    unuse element remove

--- scripts/commands/watch.js
+++ scripts/commands/watch.js
@@ -2,11 +2,24 @@ export default function() {
   let bb = this;
   let args = [].slice.call(arguments);
   let event = args.shift();
-  let context = {};
   if (event && event.isBBEvent) {
     let callback = args.shift();
+    let watchedElements = [];
     setInterval(() => {
-      event.handle(context, callback);
+      let elements = event.getElements();
+      if (elements.length == 0) {
+        watchedElements = [];
+        return;
+      }
+      elements.forEach((element) => {
+        if (watchedElements.indexOf(element) < 0) {
+          watchedElements.push(element);
+          event.handle(element, callback);
+        }
+      });
+      watchedElements = watchedElements.filter((watched) => {
+        return elements.indexOf(watched) >= 0;
+      });
     }, 50);
   }
 
