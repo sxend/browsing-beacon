f4f5b37ebbc764946ef5018e830f172ac0db245e
sxend <arimitsu.k+github@gmail.com>
2016-01-29 15:40:27 +0900

    anonymous id randamize

--- scripts/config/defaults.js
+++ scripts/config/defaults.js
@@ -1,5 +1,5 @@
 export default {
-  id: `UA-anonymous-0`, // FIXME idのデフォルト生成方法検討
+  id: `UA-${Date.now().toString(32) + Math.random().toString(32).substring(2)}-0`, // FIXME idのデフォルト生成方法検討
   plugins: [],
   endpoint: '//0.0.0.0/beacon',
   transport: 'auto',
