5bdb1a27b52b8785273026fa33f160b80ca3b6a7
sxend <arimitsu.k+github@gmail.com>
2016-02-01 16:22:57 +0900

    migrate start typescript

---
+++ scripts/index.ts
@@ -0,0 +1,81 @@
+declare var window: Window;
+declare var commands: any;
+import {
+isFunction,
+isString
+}
+from './utils/type-check.ts';
+
+interface BB {
+  (...args: any[]): void;
+  q: any[];
+  l: number;
+}
+
+var name: string = window['BrowsingBeaconObject'];
+var __bb: BB = window[name];
+var bb: BB = <BB> function(...args: any[]): void {
+  var command: any = args.shift();
+  if (!command) {
+    return;
+  }
+  if (isFunction(command)) {
+    command.apply(bb, args);
+    return;
+  }
+  if (isString(command)) {
+    var handler = commands[command];
+    if (isFunction(handler)) {
+      handler.apply(bb, args);
+    }
+  }
+};
+bb.l = __bb.l;
+
+window[name] = bb;
+__bb.q.forEach(function(queuedArguments) {
+  bb.apply(null, queuedArguments);
+});
+
+// import commands from './commands';
+// import events from './events';
+// import {
+//   isFunction,
+//   isString
+// }
+// from './utils/type-check';
+//
+// let name = window['BrowsingBeaconObject'];
+// let oldbb = window[name];
+//
+// let bb = function() {
+//   let args = [].slice.call(arguments);
+//   let command = args.shift();
+//   if (!command) {
+//     return;
+//   }
+//   if (isFunction(command)) {
+//     command.apply(bb, args);
+//     return;
+//   }
+//   if (isString(command)) {
+//     let handler = commands[command];
+//     if (isFunction(handler)) {
+//       handler.apply(bb, args);
+//     }
+//   }
+// };
+//
+// bb.l = oldbb.l;
+// bb.__ENVIRONMENT__ = oldbb.__ENVIRONMENT__;
+// bb.ev = events;
+//
+// bb.isProduction = function() {
+//   return !bb.__ENVIRONMENT__ || bb.__ENVIRONMENT__ == 'prd';
+// }
+// bb.log = bb.isProduction() ? function() {} : console.log.bind(console);
+// window[name] = bb;
+//
+// oldbb.q.forEach(function(queuedArguments) {
+//   bb.apply(null, queuedArguments);
+// });
--- scripts/utils/type-check.ts
+++ scripts/utils/type-check.ts
@@ -1,15 +1,15 @@
-export function isFunction(o) {
+export function isFunction(o: any): boolean {
   return "function" == typeof o;
 }
 
-export function isNumber(o) {
+export function isNumber(o: any): boolean {
   return "number" == typeof o;
 }
 
-export function isArray(o) {
+export function isArray(o: any): boolean {
   return "[object Array]" == Object.prototype.toString.call(Object(o));
 }
 
-export function isString(o) {
+export function isString(o: any): boolean {
   return void 0 != o && -1 < (o.constructor + "").indexOf("String");
 }
