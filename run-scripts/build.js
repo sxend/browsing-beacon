44f3a9860a0e1dfd0a5bec32cec80cabd22a2ac2
sxend <arimitsu.k+github@gmail.com>
2016-02-02 11:39:46 +0900

    move config

--- run-scripts/build.js
+++ run-scripts/build.js
@@ -1,41 +1,7 @@
-var path = require('path');
 var webpack = require('webpack');
-
 var toProduction = !!process.env['TO_PRODUCTION'];
-var minifyPlugin = new webpack.optimize.UglifyJsPlugin({
-  compress: {
-    warnings: false
-  },
-  mangle: {
-    except: []
-  }
-});
+var config = require('./webpack.config.js')(toProduction);
 
-var plugins = toProduction ? [minifyPlugin] : [];
-
-var config = {
-  context: path.join(__dirname, "../src"),
-  entry: "./index.ts",
-  output: {
-    path: path.join(__dirname, "../dist"),
-    filename: "bb.js"
-  },
-  plugins: plugins,
-  resolve: {
-    extensions: ['', '.webpack.js', '.ts', '.js']
-  },
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel?presets[]=es2015'
-    }, {
-      test: /\.ts$/,
-      loader: 'ts-loader'
-    }]
-  },
-  devtool: "#source-map"
-};
 webpack(config, function(err, stats) {
   if (err) {
     throw err;
---
+++ run-scripts/webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = function(toProduction) {
+  var plugins = [];
+  if (toProduction) {
+    plugins.push(uglifyPlugin());
+  }
+  return {
+    context: path.join(__dirname, "../src"),
+    entry: "./index.ts",
+    output: {
+      path: path.join(__dirname, "../dist"),
+      filename: "bb.js"
+    },
+    plugins: plugins,
+    resolve: {
+      extensions: ['', '.webpack.js', '.ts', '.js']
+    },
+    module: {
+      loaders: [{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel?presets[]=es2015'
+      }, {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }]
+    },
+    devtool: "#source-map"
+  };
+}
+
+function uglifyPlugin() {
+  return new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    },
+    mangle: {
+      except: []
+    }
+  });
+}
