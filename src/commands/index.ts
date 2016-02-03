62adf85b2a47fab3dd67a3314110990371999dd4
sxend <arimitsu.k+github@gmail.com>
2016-02-03 13:54:38 +0900

    add set command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,9 +1,11 @@
 import create from './create';
 import send from './send';
 import watch from './watch';
+import set from './set';
 
 export default {
   create: create,
   send: send,
   watch: watch,
+  set: set
 }
