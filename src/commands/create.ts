c1efe880d2a63f64f4c1129e5d3669d5b7afe5ba
sxend <arimitsu.k+github@gmail.com>
2016-02-03 11:00:40 +0900

    refactor command option

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,15 +1,19 @@
 declare var window: any;
 import {BBObject} from '../index.ts';
 import Config from '../config/index.ts';
-import {isString} from '../utils/type-check.ts';
+import {isString, toObject} from '../utils/type-check.ts';
 
-export default function create(id: string, option: any): void {
+export default function create(id: string, endpoint: any, option: any): void {
   'use strict';
   var bb: BBObject = this;
   if (!isString(id)) {
     throw new Error("id is required.");
   }
   bb.id = id;
+  option = toObject(endpoint) || toObject(option) || {};
+  if (isString(endpoint)) {
+    option.endpoint = endpoint;
+  }
   Config.setConfig(option);
   var config: any = Config.getConfig();
   config.plugins.forEach(function(url, index) {
