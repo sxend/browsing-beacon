feb7fc9a1970e610baea060a9f443f900c24f4af
sxend <arimitsu.k+github@gmail.com>
2016-02-04 12:23:11 +0900

    fix set command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,11 +1,9 @@
 import create from './create';
 import send from './send';
 import watch from './watch';
-import set from './set';
 
 export default {
   create: create,
   send: send,
-  watch: watch,
-  set: set
+  watch: watch
 }
