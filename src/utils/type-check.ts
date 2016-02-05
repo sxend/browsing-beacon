04f8573e38e3864222c23f5c8ffebe24ae333882
sxend <arimitsu.k+github@gmail.com>
2016-02-05 18:06:53 +0900

    change methods

--- src/utils/type-check.ts
+++ src/utils/type-check.ts
@@ -1,22 +1,22 @@
 
 export function isFunction(o: any): boolean {
   'use strict';
-  return "function" === typeof o;
+  return o !== void 0 && o !== null && "[object Function]" === Object.prototype.toString.call(Object(o));
 }
 
 export function isNumber(o: any): boolean {
   'use strict';
-  return "number" === typeof o;
+  return o !== void 0 && o !== null && "[object Number]" === Object.prototype.toString.call(Object(o));
 }
 
 export function isArray(o: any): boolean {
   'use strict';
-  return "[object Array]" === Object.prototype.toString.call(Object(o));
+  return o !== void 0 && o !== null && "[object Array]" === Object.prototype.toString.call(Object(o));
 }
 
 export function isString(o: any): boolean {
   'use strict';
-  return void 0 !== o && -1 < (o.constructor + "").indexOf("String");
+  return o !== void 0 && o !== null && "[object String]" === Object.prototype.toString.call(Object(o));
 }
 
 export function isObject(o: any): boolean {
