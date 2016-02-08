4fa2657aa71624af58f2fcc34e6f33ffaf168799
sxend <arimitsu.k+github@gmail.com>
2016-02-08 16:31:55 +0900

    add model

--- src/model.ts
+++ src/model.ts
@@ -1,3 +1,9 @@
 export default class Model {
-
+  data: any = [];
+  get(key: string): any {
+    return this.data[key];
+  }
+  set(key: string, value: any): void {
+    this.data[key] = value;
+  }
 }
