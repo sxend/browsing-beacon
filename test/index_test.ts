3ef2401788428936ded484f0bcb30e7178b85878
sxend <arimitsu.k+github@gmail.com>
2016-02-09 20:46:18 +0900

    clear test

--- test/index_test.ts
+++ test/index_test.ts
@@ -1,10 +1,8 @@
 declare var describe: any;
 declare var it: any;
 declare var expect: any;
-import {isString} from '../src/utils/type-checker';
 
 describe("test", () => {
   it('sample', function() {
-    expect(isString("")).to.be.true;
   });
 });
