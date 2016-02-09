e5632c4ab40b03213ef7eecdc8cea91168e2b980
sxend <arimitsu.k+github@gmail.com>
2016-02-09 11:54:11 +0900

    add prefix search

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -2,8 +2,11 @@
 // http://stackoverflow.com/questions/280634/endswith-in-javascript
 export module Strings {
   'use strict';
-  export function endsWith(str, suffix) {
+  export function startsWith(str, prefix) {
+    return str.lastIndexOf(prefix, 0) === 0;
+  }
 
+  export function endsWith(str, suffix) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
   }
 }
