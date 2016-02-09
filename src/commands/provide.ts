6b05232dbfb3ed280a49ff53a7e3d266e0f6a331
sxend <arimitsu.k+github@gmail.com>
2016-02-09 14:56:53 +0900

    url load to provide

--- src/commands/provide.ts
+++ src/commands/provide.ts
@@ -1,15 +1,47 @@
 import Tracker from '../tracker';
 import {TypeChecker} from '../utils/type-checker';
 import {BrowsingBeacon} from '../browsing-beacon';
-export default function provide(tracker: Tracker, pluginName: string, PluginConstructor: any) {
+import {Strings} from '../utils/strings';
+
+export default function provide(tracker: Tracker, pluginName: string, constructorOrUrl: any, callback?: (PluginConstructor) => void) {
   'use strict';
   var bb: BrowsingBeacon = this;
   if (!TypeChecker.isString(pluginName)) {
     throw new Error('pluginName must be string');
   }
-  if (!TypeChecker.isFunction(PluginConstructor)) {
-    throw new Error('PluginConstructor must be function');
-  }
   bb.plg = bb.plg || {};
-  bb.plg[pluginName] = PluginConstructor;
+  if (TypeChecker.isFunction(constructorOrUrl)) {
+    bb.plg[pluginName] = constructorOrUrl;
+  } else if (Strings.startsWith(constructorOrUrl, '//')) {
+    loadPluginFromUrl(bb, pluginName, constructorOrUrl);
+  }
+}
+var queue = [];
+function loadPluginFromUrl(bb: BrowsingBeacon, pluginName: string, pluginUrl: string) {
+  'use strict';
+  function next() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = pluginUrl;
+    var beforeTag = document.getElementsByTagName('script')[0];
+    beforeTag.parentNode.insertBefore(script, beforeTag);
+    window['__BBPluginCallback'] = function(generator) {
+      generator(bb, function(err, PluginConstructor) {
+        if (err) {
+          throw err;
+        }
+        bb.plg[pluginName] = PluginConstructor;
+      });
+      if (queue.length > 0) {
+        queue.shift()();
+      } else { // queue 消化完了
+        window['__BBPluginCallback'] = void 0;
+      }
+    };
+  }
+  if (!TypeChecker.isFunction(window['__BBPluginCallback'])) {
+    next();
+  } else {
+    queue.push(next);
+  }
 }
--- src/commands/require.ts
+++ src/commands/require.ts
@@ -1,52 +1,22 @@
 import Tracker from '../tracker';
 import {TypeChecker} from '../utils/type-checker';
 import {BrowsingBeacon} from '../browsing-beacon';
-import provide from './provide';
 
 // requireは予約語のため、関数名省略
-export default function(tracker: Tracker, pluginName: string, pluginUrl: string, pluginOption: any = {}) {
+export default function(tracker: Tracker, pluginName: string, pluginOption: any = {}) {
   'use strict';
   var bb: BrowsingBeacon = this;
   if (!TypeChecker.isString(pluginName)) {
     throw new Error("pluginName is not string.");
   }
-  if (TypeChecker.isObject(pluginUrl)) {
-    pluginOption = pluginUrl;
-  }
-  if (TypeChecker.isString(pluginUrl)) {
-    // urlでロード
-    registerPluginFromUrl(bb, tracker, pluginName, pluginUrl, pluginOption);
-  } else {
-    registerPlugin(bb, tracker, pluginName, pluginOption);
-  }
+  registerPlugin(bb, tracker, pluginName, pluginOption);
 }
 
 function registerPlugin(bb: BrowsingBeacon, tracker: Tracker, pluginName: string, pluginOption: any) {
   'use strict';
   var PluginConstructor = bb.plg[pluginName];
   if (TypeChecker.isFunction(PluginConstructor)) {
-    var plugin = new PluginConstructor(pluginOption);
+    var plugin = new PluginConstructor(tracker, pluginOption);
     tracker.set(pluginName, plugin);
   }
-}
-
-function registerPluginFromUrl(bb: BrowsingBeacon, tracker: Tracker, pluginName: string, pluginUrl: string, pluginOption: any) {
-  'use strict';
-  var script = document.createElement('script');
-  script.async = true;
-  script.src = pluginUrl;
-  var beforeTag = document.getElementsByTagName('script')[0];
-  beforeTag.parentNode.insertBefore(script, beforeTag);
-  var pluginCallbackName = '__BBPluginCallback_' + tracker.get("name");
-  window[pluginCallbackName] = (window[pluginCallbackName] || function(generator) {
-    generator(bb, tracker, function(err, PluginConstructor) {
-      if (err) {
-        throw err;
-      }
-      if (TypeChecker.isFunction(PluginConstructor)) {
-        provide.call(bb, tracker, pluginName, PluginConstructor);
-        registerPlugin(bb, tracker, pluginName, pluginOption);
-      }
-    });
-  });
 }
