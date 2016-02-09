7133996dac088c87985dd6890102c86d399d55b7
sxend <arimitsu.k+github@gmail.com>
2016-02-09 11:03:21 +0900

    rename args

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -3,35 +3,35 @@ https://developer.mozilla.org/ja/docs/Web/API/Document/cookie
 */
 
 export default class Cookies {
-  public static getItem(sKey: string): string {
-    if (!sKey || !this.hasItem(sKey)) { return null; }
-    return decodeURIComponent(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
+  public static getItem(key: string): string {
+    if (!key || !this.hasItem(key)) { return null; }
+    return decodeURIComponent(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + encodeURIComponent(key).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
   }
-  public static setItem(sKey, sValue, vEnd?, sPath?, sDomain?, bSecure?): void {
-    if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return; }
-    var sExpires = "";
-    if (vEnd) {
-      switch (vEnd.constructor) {
+  public static setItem(key, value, expireSeconds?, path?, domain?, secureMode?): void {
+    if (!key || /^(?:expires|max\-age|path|domain|secure)$/i.test(key)) { return; }
+    var expireString = "";
+    if (expireSeconds) {
+      switch (expireSeconds.constructor) {
         case Number:
-          sExpires = vEnd === Infinity ? "; expires=Tue, 19 Jan 2038 03:14:07 GMT" : "; max-age=" + vEnd;
+          expireString = expireSeconds === Infinity ? "; expires=Tue, 19 Jan 2038 03:14:07 GMT" : "; max-age=" + expireSeconds;
           break;
         case String:
-          sExpires = "; expires=" + vEnd;
+          expireString = "; expires=" + expireSeconds;
           break;
         case Date:
-          sExpires = "; expires=" + vEnd.toGMTString();
+          expireString = "; expires=" + expireSeconds.toGMTString();
           break;
       }
     }
-    document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+    document.cookie = encodeURIComponent(key) + "=" + encodeURIComponent(value) + expireString + (domain ? "; domain=" + domain : "") + (path ? "; path=" + path : "") + (secureMode ? "; secure" : "");
   }
-  public static removeItem(sKey, sPath?): void {
-    if (!sKey || !this.hasItem(sKey)) { return; }
-    document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sPath ? "; path=" + sPath : "");
+  public static removeItem(key, path?): void {
+    if (!key || !this.hasItem(key)) { return; }
+    document.cookie = encodeURIComponent(key) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (path ? "; path=" + path : "");
 
   }
-  public static hasItem(sKey): boolean {
-    return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+  public static hasItem(key): boolean {
+    return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(key).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
   }
   public static keys(): string[] {
     var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
