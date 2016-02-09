14443574bc7d6e773799bf695e5b4674a60bdfdb
sxend <arimitsu.k+github@gmail.com>
2016-02-09 12:55:58 +0900

    fix refactor

--- src/commands/provide.ts
+++ src/commands/provide.ts
@@ -2,6 +2,7 @@ import Tracker from '../tracker';
 import {TypeChecker} from '../utils/type-checker';
 import {BrowsingBeacon} from '../browsing-beacon';
 export default function provide(tracker: Tracker, pluginName: string, PluginConstructor: any) {
+  'use strict';
   var bb: BrowsingBeacon = this;
   if (!TypeChecker.isString(pluginName)) {
     throw new Error('pluginName must be string');
